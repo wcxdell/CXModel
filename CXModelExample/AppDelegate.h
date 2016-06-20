22a03603467e0cf815c6aeb3d7a830171f97fdff
wcxdell <wcxdell@wcxdelldeMacBook-Pro.local>
2016-06-20 11:54:26 +0800

    init

---
+++ CXModelExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CXModelExample
+//
+//  Created by wcxdell on 16/6/20.
+//  Copyright © 2016年 wcxdell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CXModelExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CXModelExample
+//
+//  Created by wcxdell on 16/6/20.
+//  Copyright © 2016年 wcxdell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
